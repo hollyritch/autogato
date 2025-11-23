85e1aec0944722e6efac5ef6ef9b64752229586b
hollyritch <rgolnik@posteo.de>
2025-11-23 00:28:48 -0300

    Performance test

--- completeRunner.py
+++ completeRunner.py
@@ -11,7 +11,7 @@ speciesXMLDict = {}
 for k in tqdm(range(len(speciesList)), desc="Species"):
     species = speciesList[k]    
     print(species)
-    if species != "EColiDH5Alpha":
+    if species != "MethanosarcinaBarkeri":
         continue
     if species == ".DS_Store":
         continue
